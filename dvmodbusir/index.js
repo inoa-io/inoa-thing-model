70f1c13670c61b18f63596fd75f1f2a8813e4b51
Rico Pahlisch <rico.pahlisch@grayc.de>
2023-07-12 16:01:10 +0200

    update

--- dvmodbusir/index.js
+++ dvmodbusir/index.js
@@ -1,7 +1,17 @@
 import {Buffer} from 'buffer';
 
 export function translateMessage(thing, body, headers) {
-    let result = {};
+    let result = [];
+    try {
+        result.push({
+            urn: body.urn,
+            thingId: thing.thingId,
+        });
+    } catch (e) {
+        result.error = e.message;
+        console.error(e);
+
+    }
     return result;
 }
 
