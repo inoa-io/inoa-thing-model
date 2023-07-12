7df284dadc2b00a18ed351cfb0cc322e0fc5dfc4
Rico Pahlisch <rico.pahlisch@grayc.de>
2023-07-12 12:48:50 +0200

    add shellyplugs

---
+++ shellyplugs/index.js
@@ -0,0 +1,12 @@
+import {Buffer} from 'buffer';
+
+export function translateMessage(thing, body, headers) {
+    let result = {};
+    return result;
+}
+
+
+
+export function generateSatelliteConfig(gateway, things) {
+    return {};
+}
